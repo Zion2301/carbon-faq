38b54ac6b1345659b255601369085a6b3c928862
Zion <omogbemezion@gmail.com>
2024-05-30 12:34:54 -0700

    first commit

--- carbon faq.js
+++ carbon faq.js
@@ -1,17 +1,21 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     const eachitem = document.querySelectorAll(".each-div");
 
     eachitem.forEach(element => {
         const thequestion = element.querySelector(".question-div");
-        const theanswer = element.querySelector(".answer");
-        const btn = element.querySelector(".toggle-sign");
 
-        thequestion.addEventListener('click', (event)=> {
-            // Check if the answer is currently visible or hidden
+        thequestion.addEventListener('click', function(event) {
             event.preventDefault();
-            const isAnswerVisible = window.getComputedStyle(theanswer).display !== "none";
+            
+            // 'this' refers to the clicked '.question-div'
+            const parent = this.closest('.each-div');
+            const theanswer = parent.querySelector(".answer");
+            const btn = parent.querySelector(".toggle-sign");
 
-            if (isAnswerVisible) {
+            // Check if the answer is currently visible or hidden
+
+            if (theanswer.style.display === "block"  || theanswer.style.display === "") {
                 theanswer.style.display = "none";
                 btn.textContent = "+";
                 btn.style.transform = "rotate(0deg)";
